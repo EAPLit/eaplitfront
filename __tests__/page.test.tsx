d0f211d7715822c363a0e22a99f1d708665b9c2f
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-12 14:13:12 +0900

    test: add test for login button display and routing

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,12 +1,19 @@
 import { expect, describe, it, vi, beforeEach, afterEach } from 'vitest';
 import { render, screen, waitFor, cleanup } from '@testing-library/react';
 import Home from '../app/page';
+import userEvent from '@testing-library/user-event';
+import { useRouter } from 'next/navigation';
 
 // Mock the authOnAppLoad function
 vi.mock('@services/auth', () => ({
     authOnAppLoad: vi.fn(),
 }));
 import { authOnAppLoad } from '@services/auth';
+
+// Mock the next/navigation
+vi.mock("next/navigation", () => ({
+    useRouter: vi.fn(),
+}));
 
 describe("Landing Page", () => {
     beforeEach(() => {
@@ -30,5 +37,20 @@ describe("Landing Page", () => {
         await waitFor(() => {
             expect(authOnAppLoad).toHaveBeenCalledTimes(2);
         });
+    });
+
+    it("should render a login and a registration button", () => {
+        expect(screen.getByRole("button", { name: '/login/i' })).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: '/register/i' })).toBeInTheDocument();
+    });
+
+    it("should navigate to the login page when the login button is clicked", async () => {
+        const mockPush = vi.fn();
+        (useRouter as vi.Mock).mockReturnvalue({ push: mockPush });
+
+        const loginButton = screen.getByRole("button", { name: /login/i });
+        await userEvent.click(loginButton);
+
+        expect(mockPush).toHaveBeenCalledWith("/login");
     });
 });
