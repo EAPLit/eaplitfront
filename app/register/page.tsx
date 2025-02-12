83951f446669b5a99ade1f0097fc93d1694d6f16
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-12 17:01:56 +0900

    feat: add registration page passing all tests

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,39 @@
 const Register = () => {
     return (
         <div>
+            <form aria-labelledby="register-heading">
+                <h1 id="register-heading">Register</h1>
+                <section aria-label="Register Panel">
+                    <div>
+                        <label htmlFor="your-name">Name</label>
+                        <input id="your-name" type="text" placeholder="Your name" />
+                    </div>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input id="username" type="text" placeholder="Username" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input id="email" type="email" placeholder="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type="password" placeholder="password" />
+                    </div>
+                    <div>
+                        <label htmlFor="confirm-password">Confirm password</label>
+                        <input id="confirm-password" type="password" placeholder="confirm password" />
+                    </div>
+                    <div>
+                        <button type="submit">Register</button>
+                    </div>
+                </section>
+            </form>
+            <section aria-label="Back to login">
+                <div>
+                    <p>To login</p>
+                </div>
+            </section>
 
         </div>
     );
