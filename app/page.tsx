c9cf327c885f8b18e087bf0d3be5725c032f1623
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-12 23:05:35 +0900

    fix: remove unused navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { useEffect } from 'react';
 import { authOnAppLoad } from '@services/auth';
 import { useRouter } from 'next/navigation';
-import Navbar from './components/Navbar';
 import "./styles/home.scss";
 
 export default function Home() {
