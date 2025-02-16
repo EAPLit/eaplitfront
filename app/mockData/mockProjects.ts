fa5ce04310b753317c13e63219e39490a78d6ec2
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-16 09:36:44 +0900

    chore: add mock data for projects

---
+++ app/mockData/mockProjects.ts
@@ -0,0 +1,12 @@
+export const MockProjects = [
+    {
+        id: 1,
+        name: "United Nations",
+        textId: 1
+    },
+    {
+        id: 2,
+        name: "Global Warming",
+        textId: 2
+    }
+]
