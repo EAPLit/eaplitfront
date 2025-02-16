ee7b854bac7f004b0a42bc5151d683ecb13885e4
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-16 09:57:12 +0900

    chore: add mock task types

---
+++ app/mockData/mockTaskTypes.ts
@@ -0,0 +1,72 @@
+export const mockTaskType = {
+    taskTypesArray: [
+        {
+            taskTypeId: "1",
+            taskTypeName: "Compound Sentences"
+        },
+        {
+            taskTypeId: "2",
+            taskTypeName: "Conclusion Sentences"
+        },
+        {
+            taskTypeId: "3",
+            taskTypeName: "Paraphrasing"
+        },
+        {
+            taskTypeId: "4",
+            taskTypeName: "Thesis Statements"
+        },
+        {
+            taskTypeId: "5",
+            taskTypeName: "Transition Sentences"
+        },
+        {
+            taskTypeId: "6",
+            taskTypeName: "Subject Verb Object"
+        },
+        {
+            taskTypeId: "7",
+            taskTypeName: "Citation"
+        },
+        {
+            taskTypeId: "8",
+            taskTypeName: "Integrating"
+        },
+        {
+            taskTypeId: "10",
+            taskTypeName: "Vocabulary and Synonymy"
+        },
+        {
+            taskTypeId: "11",
+            taskTypeName: "Supporting Sentences"
+        },
+        {
+            taskTypeId: "12",
+            taskTypeName: "Basic Skills"
+        },
+        {
+            taskTypeId: "13",
+            taskTypeName: "Conclusion Paragraphs"
+        },
+        {
+            taskTypeId: "14",
+            taskTypeName: "Essay Organization"
+        },
+        {
+            taskTypeId: "15",
+            taskTypeName: "Topic Sentences"
+        },
+        {
+            taskTypeId: "16",
+            taskTypeName: "Introduction Sentences"
+        },
+        {
+            taskTypeId: "17",
+            taskTypeName: "Sentence Connectors"
+        },
+        {
+            taskTypeId: "17",
+            taskTypeName: "Complex Sentences"
+        },
+    ]
+}
