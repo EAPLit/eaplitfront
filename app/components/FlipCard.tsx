e501f03d49a70027ab8442e7d08c15ca7e0a290e
Daniel Parsons <parsonsdwp@gmail.com>
2025-07-11 10:44:10 +0900

    feat: set status for flipping the card

--- app/components/FlipCard.tsx
+++ app/components/FlipCard.tsx
@@ -13,22 +13,34 @@ type FlipCardProps = {
 
 const FlipCard: React.FC<FlipCardProps> = ({ title, listAndDetails }) => {
 
+    const [showFront, setShowFront] = useState(true);
+
     return (
-        <div className="flipcard-container">
-            <div className="flipcard-header">
-                <div className="flipcard-title">
+        <div className="flipcard-container" onClick={() => setShowFront(!showFront)}>
+            <div className={`flipcard-inner ${showFront ? '' : 'flipped'}`}>
+                {/* Front of the card */}
+                <div className="flipcard-front">
+                    <div className="flipcard-header">
+                        <div className="flipcard-title">
+
+                        </div>
+                        <div className="flipcard-title-underline">
+
+                        </div>
+                    </div>
+                    <div className="flipcard-body">
+
+                    </div>
+                    <div className="flipcard-footer">
+
+                    </div>
+                </div>
+                {/* Back of the card */}
+                <div className="flipcard-back">
 
                 </div>
-                <div className="flipcard-title-underline">
-
-                </div>
             </div>
-            <div className="flipcard-body">
-
-            </div>
-            <div className="flipcard-footer">
-
-            </div>
+            
         </div>
     )
 }
